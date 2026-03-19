1822eabfe28761bbdcba581f96de8a44d48d6f1f
2006avni <avnimeshram2@gmail.com>
2026-03-19 18:45:58 +0530

    Added the file
    Added the file

---
+++ Resort.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Resort {
+    int RNo;
+    String Name;
+    float Charges;
+    int Days;
+
+    float Compute() {
+        float amount = Days * Charges;
+        if (amount > 11000) {
+            amount = 1.02f * amount;
+        }
+        return amount;
+    }
+
+    void Getinfo() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Room No: ");
+        RNo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        Name = sc.nextLine();
+
+        System.out.print("Enter Charges per day: ");
+        Charges = sc.nextFloat();
+
+        System.out.print("Enter Days: ");
+        Days = sc.nextInt();
+    }
+
+    void DispInfo() {
+        System.out.println("Room No: " + RNo);
+        System.out.println("Name: " + Name);
+        System.out.println("Charges: " + Charges);
+        System.out.println("Days: " + Days);
+        System.out.println("Total Amount: " + Compute());
+    }
+
+    public static void main(String[] args) {
+        Resort r = new Resort();
+        r.Getinfo();
+        r.DispInfo();
+    }
+}
